20046e6e44395a89829ea880d488819bc87376d7
thomasdegisi <114692675+thomasdegisi@users.noreply.github.com>
2022-12-14 23:50:26 -0600

    Improved quick links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,10 +23,17 @@ import { QuickLinksComponent } from './quick-links/quick-links.component';
     ToolbarComponent,
     BrandComponent,
     AnnouncementsComponent,
-    QuickLinksComponent
+    QuickLinksComponent,
   ],
   imports: [
-    BrowserModule, HttpClientModule, MatButtonModule, MatCardModule, MatIconModule, MatToolbarModule
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -35,16 +44,28 @@ export class AppModule {
     private domSanitizer: DomSanitizer
   ) {
     this.matIconRegistry.addSvgIcon(
+      'angular',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/angular.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
       'github',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/github.svg'
       )
-    );  
+    );
     this.matIconRegistry.addSvgIcon(
       'linked-in',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/linked-in.svg'
       )
-    );  
+    );
+    this.matIconRegistry.addSvgIcon(
+      'material',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/material.svg'
+      )
+    );
   }
 }
